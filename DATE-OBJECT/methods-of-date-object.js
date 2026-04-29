27389e1d653f39f906eb6609a462cf53dcdc732d
deepak0deepz <deepakcs2k4@gmail.com>
2026-04-29 09:51:48 +0530

    Updated project files

---
+++ DATE-OBJECT/methods-of-date-object.js
@@ -0,0 +1,15 @@
+// methods-of-date-object.js
+
+var curdate = date.getDate();  // Returns the day of the month (1-31) for the specified date according to local time.
+console.log(curdate);          // Outputs the current day of the month
+
+var month = date.getMonth();  // Returns the month (0-11) for the specified date according to local time. January is 0, February is 1, and so on.
+console.log(month);
+
+// var date=date.
+
+var minutes = date.getMinutes();  // Returns the minutes (0-59) for the specified date according to local time.
+console.log(minutes);
+
+var seconds = date.getSeconds();  // Returns the seconds (0-59) for the specified date according to local time.
+console.log(seconds);
