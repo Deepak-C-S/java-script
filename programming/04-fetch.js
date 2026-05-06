158f2ddabb245aec0e6db0c9e2f957880c4bb379
deepak0deepz <deepakcs2k4@gmail.com>
2026-05-06 10:24:51 +0530

    js

--- programming/04-fetch.js
+++ programming/04-fetch.js
@@ -31,3 +31,20 @@ async function fun1() {
 }
 fun1();
 
+//! using fetch with post request
+
+fetch("https://fakestoreapi.com/products", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        title: "New Product",
+        price: 19.99,
+        description: "This is a new product",
+    })
+})
+.then((res) => res.json())
+.then((jsonData) => {
+    console.log(jsonData);
+})
