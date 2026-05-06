61990b17f8f0d87ec0111fbd1e2103fa0ac5645f
deepak0deepz <deepakcs2k4@gmail.com>
2026-05-06 09:43:48 +0530

    js

--- programming/03-promise.js
+++ programming/03-promise.js
@@ -1,3 +1,24 @@
 //! 03-promise.js
+// These are the way to handle asynchronous operations in js    
+// A promise is a js object that links producing code and consuming code that represents a value that may be available now, in the future, or never.
 
-// A promise is a 
+//: The promise has 3 states :
+//* 1. Pending : Initial state, neither fulfilled nor rejected.
+//* 2. Fulfilled : The operation completed successfully, and the promise has a resolved value.
+//* 3. Rejected : The operation failed, and the promise has a reason for the failure (error).    
+
+//|                              PROMISE
+//|                                 |
+//|    ---------------------------------------------------------
+//|    |                            |                           |
+//|   Fulfilled                  pending                     Rejected
+//|    |                            |                           |
+//|    V                            V                           V
+//|   resolve()                                             reject()
+//|    |                                                        |
+//|    V                                                        V
+//|  then()                                                   catch()
+
+
+
+
